8ced0c1879d60f4eab00061323b8550c74507310
Hodobox <samkogursky@gmail.com>
2024-06-08 17:10:09 +0200

    format world.py

--- code/world.py
+++ code/world.py
@@ -1,6 +1,15 @@
-class Hero():
-    def __init__(self, x : int, y : int, base_speed: int, base_power: int,
-                base_range: int, speed_coef: int, power_coef: int, range_coef: int):
+class Hero:
+    def __init__(
+        self,
+        x: int,
+        y: int,
+        base_speed: int,
+        base_power: int,
+        base_range: int,
+        speed_coef: int,
+        power_coef: int,
+        range_coef: int,
+    ):
         self.x = x
         self.y = y
         self.base_speed = base_speed
@@ -12,20 +21,22 @@ class Hero():
         self.xp = 0
         self.gold = 0
 
-class Monster():
-    def __init__(self, x : int,y : int, hp: int, gold: int, xp: int):
+
+class Monster:
+    def __init__(self, x: int, y: int, hp: int, gold: int, xp: int):
         self.x = x
         self.y = y
         self.hp = hp
         self.gold = gold
         self.xp = xp
 
-class World():
-    def __init__(self, height : int, width : int, hero: Hero, monsters: list[Monster], turns: int):
+
+class World:
+    def __init__(
+        self, height: int, width: int, hero: Hero, monsters: list[Monster], turns: int
+    ):
         self.height = height
         self.width = width
         self.hero = hero
-        self.monsters = monsters    
+        self.monsters = monsters
         self.turns = turns
-
-        
